bdeb9c3d604df849cbe1471eb376751d6b67aef9
Nanio <111894274+NanioY@users.noreply.github.com>
2023-08-16 21:28:11 +0200

    fonts now load in first. Stylized Navigation bar.

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'react-native';
 import Start from './screens/Start';
 import ItemDetails from './screens/ItemDetails';
 import Cart from './screens/Cart';
@@ -10,32 +11,65 @@ import { CartItemProvider } from './Components/CartItemContext';
 // Create a stack navigator instance
 const Stack = createNativeStackNavigator();
 
-// Function to load custom fonts asynchronously
-async function loadCustomFont() {
-  await Font.loadAsync({
-    'Header': require('./assets/TwilioSansMono-Bold.otf'),
-    'Body': require('./assets/Sloth-Regular.ttf'),
-  });
-}
-
 // Main app component
 export default function App() {
+  // State to track whether the fonts are loaded
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
   // Load custom fonts on app start
   useEffect(() => {
+    // Function to load custom fonts asynchronously
+    async function loadCustomFont() {
+      await Font.loadAsync({
+        'Header': require('./assets/TwilioSansMono-Bold.otf'),
+        'Body': require('./assets/Sloth-Regular.ttf'),
+      });
+
+      // Mark fonts as loaded when the async loading is done
+      setFontsLoaded(true);
+    }
+
+    // Call the font loading function
     loadCustomFont();
   }, []);
 
-  // Render the app's UI components wrapped in the CartItemProvider context
   return (
     <CartItemProvider>
-      <NavigationContainer>
-        {/* Define navigation stack screens */}
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={Start} />
-          <Stack.Screen name="Details" component={ItemDetails} />
-          <Stack.Screen name="Cart" component={Cart} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      {fontsLoaded ? (
+        <>
+          {/* Set status bar style */}
+          <StatusBar
+            barStyle="light-content"
+            backgroundColor="#050505"
+          />
+
+          {/* Main Navigation Container */}
+          <NavigationContainer>
+            {/* Stack Navigator with header styling */}
+            <Stack.Navigator
+              screenOptions={{
+                headerStyle: {
+                  backgroundColor: '#050505',
+                },
+                headerTintColor: '#F5F5F5',
+                headerTitleStyle: {
+                  fontFamily: 'Header',
+                  fontSize: 32,
+                },
+              }}
+            >
+              {/* Define screens */}
+              <Stack.Screen name="Home" component={Start} />
+              <Stack.Screen name="Details" component={ItemDetails} />
+              <Stack.Screen name="Cart" component={Cart} />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </>
+      ) : (
+        <>
+          {/* If fonts are not loaded yet, render nothing */}
+        </>
+      )}
     </CartItemProvider>
   );
 }
